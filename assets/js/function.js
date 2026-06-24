03335c2410537e1af72ef822ea3ac189164ddd5b
Durgesh Sonar <durgeshksonar05@gmail.com>
2026-06-24 19:00:58 +0530

    implement program pages, add static assets, and update navigation for Aanav Seva Foundation website

--- assets/js/function.js
+++ assets/js/function.js
@@ -163,93 +163,95 @@
     }
 
     /* Text Effect Animation */
-    if ($('.text-anime-style-1').length) {
-        let staggerAmount = 0.05,
-            translateXValue = 0,
-            delayValue = 0.5,
-            animatedTextElements = document.querySelectorAll('.text-anime-style-1');
+    if ($(window).width() > 768) {
+        if ($('.text-anime-style-1').length) {
+            let staggerAmount = 0.05,
+                translateXValue = 0,
+                delayValue = 0.5,
+                animatedTextElements = document.querySelectorAll('.text-anime-style-1');
 
-        animatedTextElements.forEach((element) => {
-            let animationSplitText = new SplitText(element, {
-                type: "chars, words"
+            animatedTextElements.forEach((element) => {
+                let animationSplitText = new SplitText(element, {
+                    type: "chars, words"
+                });
+                gsap.from(animationSplitText.words, {
+                    duration: 1,
+                    delay: delayValue,
+                    x: 20,
+                    autoAlpha: 0,
+                    stagger: staggerAmount,
+                    scrollTrigger: {
+                        trigger: element,
+                        start: "top 85%"
+                    },
+                });
             });
-            gsap.from(animationSplitText.words, {
-                duration: 1,
-                delay: delayValue,
-                x: 20,
-                autoAlpha: 0,
-                stagger: staggerAmount,
-                scrollTrigger: {
-                    trigger: element,
-                    start: "top 85%"
-                },
-            });
-        });
-    }
+        }
 
-    if ($('.text-anime-style-2').length) {
-        let staggerAmount = 0.03,
-            translateXValue = 20,
-            delayValue = 0.1,
-            easeType = "power2.out",
-            animatedTextElements = document.querySelectorAll('.text-anime-style-2');
+        if ($('.text-anime-style-2').length) {
+            let staggerAmount = 0.03,
+                translateXValue = 20,
+                delayValue = 0.1,
+                easeType = "power2.out",
+                animatedTextElements = document.querySelectorAll('.text-anime-style-2');
 
-        animatedTextElements.forEach((element) => {
-            let animationSplitText = new SplitText(element, {
-                type: "chars, words"
+            animatedTextElements.forEach((element) => {
+                let animationSplitText = new SplitText(element, {
+                    type: "chars, words"
+                });
+                gsap.from(animationSplitText.chars, {
+                    duration: 1,
+                    delay: delayValue,
+                    x: translateXValue,
+                    autoAlpha: 0,
+                    stagger: staggerAmount,
+                    ease: easeType,
+                    scrollTrigger: {
+                        trigger: element,
+                        start: "top 85%"
+                    },
+                });
             });
-            gsap.from(animationSplitText.chars, {
-                duration: 1,
-                delay: delayValue,
-                x: translateXValue,
-                autoAlpha: 0,
-                stagger: staggerAmount,
-                ease: easeType,
-                scrollTrigger: {
-                    trigger: element,
-                    start: "top 85%"
-                },
-            });
-        });
-    }
+        }
 
-    if ($('.text-anime-style-3').length) {
-        let animatedTextElements = document.querySelectorAll('.text-anime-style-3');
+        if ($('.text-anime-style-3').length) {
+            let animatedTextElements = document.querySelectorAll('.text-anime-style-3');
 
-        animatedTextElements.forEach((element) => {
-            //Reset if needed
-            if (element.animation) {
-                element.animation.progress(1).kill();
-                element.split.revert();
-            }
+            animatedTextElements.forEach((element) => {
+                //Reset if needed
+                if (element.animation) {
+                    element.animation.progress(1).kill();
+                    element.split.revert();
+                }
 
-            element.split = new SplitText(element, {
-                type: "lines,words,chars",
-                linesClass: "split-line",
-            });
-            gsap.set(element, {
-                perspective: 400
-            });
+                element.split = new SplitText(element, {
+                    type: "lines,words,chars",
+                    linesClass: "split-line",
+                });
+                gsap.set(element, {
+                    perspective: 400
+                });
 
-            gsap.set(element.split.chars, {
-                opacity: 0,
-                x: "50",
-            });
+                gsap.set(element.split.chars, {
+                    opacity: 0,
+                    x: "50",
+                });
 
-            element.animation = gsap.to(element.split.chars, {
-                scrollTrigger: {
-                    trigger: element,
-                    start: "top 90%"
-                },
-                x: "0",
-                y: "0",
-                rotateX: "0",
-                opacity: 1,
-                duration: 1,
-                ease: Back.easeOut,
-                stagger: 0.02,
+                element.animation = gsap.to(element.split.chars, {
+                    scrollTrigger: {
+                        trigger: element,
+                        start: "top 90%"
+                    },
+                    x: "0",
+                    y: "0",
+                    rotateX: "0",
+                    opacity: 1,
+                    duration: 1,
+                    ease: Back.easeOut,
+                    stagger: 0.02,
+                });
             });
-        });
+        }
     }
 
     /* Parallaxie js */
@@ -329,7 +331,9 @@
     /* Contact form validation end */
 
     /* Animated Wow Js */
-    new WOW().init();
+    new WOW({
+        mobile: false
+    }).init();
 
     /* Popup Video */
     if ($('.popup-video').length) {
